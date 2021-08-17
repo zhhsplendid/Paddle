dab0d52cacea2e36bc9c3e8c60401b5d339c8d02
zhhsplendid <zhhsplendid@163.com>
2021-08-17 13:03:09 +0000

    Fix pip version

--- paddle/fluid/compiler/scripts/piano_ci_test.sh
+++ paddle/fluid/compiler/scripts/piano_ci_test.sh
@@ -47,7 +47,7 @@ function init() {
 function parallel_test() {
     mkdir -p ${PADDLE_ROOT}/build
     cd ${PADDLE_ROOT}/build
-    pip3.7 install ${PADDLE_ROOT}/build/python/dist/*whl
+    pip install ${PADDLE_ROOT}/build/python/dist/*whl
     cp ${PADDLE_ROOT}/build/python/paddle/fluid/tests/unittests/op_test.py ${PADDLE_ROOT}/build/python
     ut_total_startTime_s=`date +%s`
 
